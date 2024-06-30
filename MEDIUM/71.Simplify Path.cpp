a145224726ab7b771e5f8fba609ecf4a185f83f7
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-06-30 12:06:15 +0600

    Create 71.Simplify Path.cpp

---
+++ MEDIUM/71.Simplify Path.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string>st;
+        string temp, res = "";
+        for(int i = 0; i<path.size(); i++){
+            temp.clear();
+            if(path[i] == '/')
+            continue;
+            while(path[i]!='/' and i<path.size()){
+                temp+=path[i];
+                i++;
+            }
+            if(temp == ".."){
+                if(!st.empty()){
+                    st.pop();
+                }    
+            }
+            else if(temp =="."){
+                continue;
+            }
+            else{
+                    st.push(temp);
+            }
+        }
+        while(!st.empty()){
+            res = "/"+st.top()+res;
+            st.pop();
+        }
+        return res.size()==0?"/":res;
+    }
+};
