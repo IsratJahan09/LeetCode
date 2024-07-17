141842bfafd45e51bddc6fcf1461918f5de1cf45
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-07-17 18:45:10 +0600

    array

---
+++ MEDIUM/274. H-Index.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(), citations.rend());
+        int i;
+        for( i = 0; i<citations.size(); i++){
+            if(citations[i]>=(i+1))
+                continue;
+            else
+                break;               
+        }
+        return i;
+    }
+};
