c55fde0a1e42fd2674efd30170f7f59a6768a8bb
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-07-30 12:24:54 +0600

    binary search

---
+++ MEDIUM/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0; int h = n-1;
+        vector<int>a(2, -1);
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(nums[mid] == target){
+               a[0] = mid;
+               h = mid-1;
+            }
+            else if(nums[mid] < target ){
+                l = mid + 1;
+            }
+            else
+            h = mid - 1;
+        }
+         l = 0;  h = n-1;
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(nums[mid] == target){
+               a[1] = mid;
+               l = mid + 1;
+            }
+            else if(nums[mid] < target ){
+                l = mid + 1;
+            }
+            else
+            h = mid - 1;
+        }
+        return a;
+    }
+};
