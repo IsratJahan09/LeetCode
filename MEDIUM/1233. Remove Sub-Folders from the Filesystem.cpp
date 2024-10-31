111287d059111be5de12b36d7f0c9d6dbb12e5c0
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-10-31 21:29:57 +0600

    Create 1233. Remove Sub-Folders from the Filesystem.cpp

---
+++ MEDIUM/1233. Remove Sub-Folders from the Filesystem.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+        vector<string>ans={folder[0]};
+        string prev = folder[0];
+
+        for(int i = 1; i<folder.size(); i++){
+            string s = folder[i];
+            if(s.find(prev+"/")!=0){
+                ans.push_back(folder[i]);
+                prev = folder[i];
+            }
+            
+         }
+        return ans;
+    }
+};
